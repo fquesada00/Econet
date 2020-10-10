900a43b06590072db40a6a60f35950bb78e2793b
leobjorkman <leobj@kth.se>
2020-10-10 19:38:34 +0200

    Added mock list when Ecopoint is clicked and field for Adress

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'package:econet/Ecopoint.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-
 void main() {
   runApp(MyApp());
 }
@@ -21,9 +20,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.green,
-
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Econet is flying high'),
@@ -63,11 +60,9 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
         title: Text(widget.title),
       ),
       body: Center(
-
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -87,20 +82,19 @@ class _MyHomePageState extends State<MyHomePage> {
                 );
               },
             ),
-            ecopointAvailable ? SafeArea(
-
-              child: ListView.builder(
-
-                itemCount: ecopoints.length,
-                itemBuilder: (context, index) {
-                  Ecopoint aux = ecopoints[index];
-                  return ListTile(
-                    title: Text('${aux.latitude},${aux.longitude}'),
-                  );
-                },),
-            )
-                :
-            Container(),
+            ecopointAvailable
+                ? SafeArea(
+                    child: ListView.builder(
+                      itemCount: ecopoints.length,
+                      itemBuilder: (context, index) {
+                        Ecopoint aux = ecopoints[index];
+                        return ListTile(
+                          title: Text('${aux.latitude},${aux.longitude}'),
+                        );
+                      },
+                    ),
+                  )
+                : Container(),
           ],
         ),
       ),
@@ -113,8 +107,8 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-Future<List<Ecopoint>> getEcopoints(double latitude, double longitude,
-    double radius) async {
+Future<List<Ecopoint>> getEcopoints(
+    double latitude, double longitude, double radius) async {
   final response = await http.get(
     'https://us-central1-econet-8552d.cloudfunctions.net/ecopoint?radius=10&latitude=-58.479677&longitude=-34.523644',
     //headers: {HttpHeaders.authorizationHeader: "Basic your_api_token_here"},
@@ -126,7 +120,6 @@ Future<List<Ecopoint>> getEcopoints(double latitude, double longitude,
 
   return parsed.map<Ecopoint>((json) => Ecopoint.fromJson(json)).toList();
 }
-
 
 class MapSample extends StatefulWidget {
   @override
@@ -141,7 +134,7 @@ class MapSampleState extends State<MapSample> {
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(-34.523644, -58.479677),
-    zoom: 14.4746,
+    zoom: 15.4746,
   );
 
   static final CameraPosition _kLake = CameraPosition(
@@ -155,14 +148,16 @@ class MapSampleState extends State<MapSample> {
     ecopoints = getEcopoints(4536456, 2345234, 5435);
 
 //    ecopoints.forEach((element) {markers.add(createMarker(element.latitude, element.longitude));});
-    markers.add(createMarker("markerDefault", -34.523274, -58.479917));
+    markers.add(
+        createMarker("markerDefault", -34.523274, -58.479917, "TesterCalle"));
     _setMarkerIcon();
     super.initState();
   }
 
   _setMarkerIcon() async {
     markerIcon = await BitmapDescriptor.fromAssetImage(
-        ImageConfiguration(devicePixelRatio: 1), 'assets/icons/recycle_icon.png'); // por alguna razon no puedo modificar el tamanio, tuve que cambiar el de la imagen manualmente
+        ImageConfiguration(devicePixelRatio: 1),
+        'assets/icons/recycle_icon.png'); // por alguna razon no puedo modificar el tamanio, tuve que cambiar el de la imagen manualmente
   }
 
   @override
@@ -171,103 +166,120 @@ class MapSampleState extends State<MapSample> {
         appBar: AppBar(
           title: Text("no se como poner la appbar default"),
         ),
-        body: Stack(
-            children: <Widget>[
+        body: Stack(children: <Widget>[
+          Container(
+              child: FutureBuilder<List<Ecopoint>>(
+            future: ecopoints,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                snapshot.data.forEach((element) {
+                  print(element);
+                  markers.add(createMarker(element.userEmail, element.longitude,
+                      element.latitude, element.adress));
+                });
+              }
 
-              Container(
-                  child: FutureBuilder<List<Ecopoint>>(
-                    future: ecopoints,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        snapshot.data.forEach((element) {
-                          print(element);
-                          markers.add(createMarker(
-                              element.userEmail, element.longitude,
-                              element.latitude));
-                        });
-                      }
+              return GoogleMap(
+                markers: markers.toSet(),
+                zoomControlsEnabled: false,
+                initialCameraPosition: _kGooglePlex,
+                onMapCreated: (GoogleMapController controller) {
+                  _controller.complete(controller);
+                },
+              );
 
-                      return GoogleMap(
-                        markers: markers.toSet(),
-                        zoomControlsEnabled: false,
-                        initialCameraPosition: _kGooglePlex,
-                        onMapCreated: (GoogleMapController controller) {
-                          _controller.complete(controller);
-                        },
-                      );
-
-                      //            if (snapshot.hasData) {
-                      //              return Text(snapshot.data.title);
-                      //            } else if (snapshot.hasError) {
-                      //              return Text("${snapshot.error}");
-                      //            }
-                      //
-                      //            // By default, show a loading spinner.
-                      //            return CircularProgressIndicator();
-                    },
-                  )
-              ),
-              Container(
-                margin: EdgeInsets.fromLTRB(200, 0, 15, 10),
-
-                child: RaisedButton(
-                  padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      SizedBox(
-                        height: 33,
-                        width: 33,
-                        child: Image.asset(
-                          'assets/icons/econet-circle-logo-white.png',
-                        ),
-                      ),
-                      Text(
-                          "RECYCLE",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 22,
-                              fontFamily: 'SFProDisplay'
-                          ),
-                          softWrap: false),
-                    ],
+              //            if (snapshot.hasData) {
+              //              return Text(snapshot.data.title);
+              //            } else if (snapshot.hasError) {
+              //              return Text("${snapshot.error}");
+              //            }
+              //
+              //            // By default, show a loading spinner.
+              //            return CircularProgressIndicator();
+            },
+          )),
+          Container(
+            margin: EdgeInsets.fromLTRB(200, 0, 15, 10),
+            child: RaisedButton(
+              padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  SizedBox(
+                    height: 33,
+                    width: 33,
+                    child: Image.asset(
+                      'assets/icons/econet-circle-logo-white.png',
+                    ),
                   ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-
-                  color: Color(0xFFA3CB8F),
-                  onPressed: () {
-                    print("asd");
-                  },
-                ),
-
-                alignment: Alignment.bottomRight,
+                  Text("RECYCLE",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontFamily: 'SFProDisplay'),
+                      softWrap: false),
+                ],
               ),
-            ])
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              color: Color(0xFFA3CB8F),
+              onPressed: () {
+                print("asd");
+              },
+            ),
+            alignment: Alignment.bottomRight,
+          ),
+        ])
 
 //      floatingActionButton: FloatingActionButton.extended(
 //        onPressed: _goToTheLake,
 //        label: Text('To the lake!'),
 //        icon: Icon(Icons.directions_boat),
 //      ),
-    );
+        );
   }
 
-  Marker createMarker(String id, double latitude, double longitude) {
+  Marker createMarker(
+      String id, double latitude, double longitude, String Adress) {
     LatLng latlng = LatLng(latitude, longitude);
     return Marker(
-      markerId: MarkerId(id),
-      position: latlng,
-      icon: markerIcon,
-      //markerIcon!=null? markerIcon:BitmapDescriptor.defaultMarker,
-      draggable: false,
-      zIndex: 1,
-    );
+        markerId: MarkerId(id),
+        position: latlng,
+        icon: markerIcon,
+        //markerIcon!=null? markerIcon:BitmapDescriptor.defaultMarker,
+        draggable: false,
+        zIndex: 1,
+        //Calling the function that does the popup
+        onTap: _showPopupMenu);
   }
 
   Future<void> _goToTheLake() async {
     final GoogleMapController controller = await _controller.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+
+  _showPopupMenu() {
+    showMenu<String>(
+      context: context,
+      position: RelativeRect.fromLTRB(50.0, 200.0, 60.0, 0),
+      //position where you want to show the menu on screen
+      items: [
+        PopupMenuItem<String>(child: const Text("Calle test 000"), value: '1'),
+        PopupMenuItem<String>(child: const Text('Para poner reciclaje'), value: '2'),
+        PopupMenuItem<String>(child: const Text('menu option 3'), value: '3'),
+      ],
+      elevation: 8.0,
+    ).then<void>((String itemSelected) {
+      if (itemSelected == null) return;
+
+      if (itemSelected == "1") {
+        //code here
+      } else if (itemSelected == "2") {
+        //code here
+      } else {
+        //code here
+      }
+    });
   }
 }
