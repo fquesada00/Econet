4202964f3d62c6794d30675009420a4532c920e1
gonzalo-arca <goarca@itba.edu.ar>
2020-10-12 20:28:52 -0300

    Arreglos en NavBar de la Homescreen, cambios esteticos chicos

--- lib/views/GMap/GMap.dart
+++ lib/views/GMap/GMap.dart
@@ -146,18 +146,7 @@ class GMapState extends State<GMap> {
   }
 }
 
-Map<int, Color> color = {
-  50: Color.fromRGBO(163, 203, 143, .1),
-  100: Color.fromRGBO(163, 203, 143, .2),
-  200: Color.fromRGBO(163, 203, 143, .3),
-  300: Color.fromRGBO(163, 203, 143, .4),
-  400: Color.fromRGBO(163, 203, 143, .5),
-  500: Color.fromRGBO(163, 203, 143, .6),
-  600: Color.fromRGBO(163, 203, 143, .7),
-  700: Color.fromRGBO(163, 203, 143, .8),
-  800: Color.fromRGBO(163, 203, 143, .9),
-  900: Color.fromRGBO(163, 203, 143, 1),
-};
+
 Future<List<Ecopoint>> getEcopoints(
     double latitude, double longitude, double radius) async {
   final response = await http.get(
