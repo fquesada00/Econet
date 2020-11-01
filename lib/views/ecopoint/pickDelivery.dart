4c6310979907dbcc21436513422a994dd4126485
leobjorkman <leobj@kth.se>
2020-11-01 14:36:58 +0100

    PickDelivery almost done, just some estethicals left

--- lib/views/ecopoint/pickDelivery.dart
+++ lib/views/ecopoint/pickDelivery.dart
@@ -1,12 +1,12 @@
 import 'package:econet/presentation/constants.dart';
 import 'package:econet/presentation/custom_icons_icons.dart';
-import 'package:econet/views/widgets/econet_chip.dart';
 import 'package:econet/views/widgets/navbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:econet/views/widgets/button1.dart';
 import 'package:tinycolor/tinycolor.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:econet/presentation/constants.dart';
 
 
 /*class PickDelivery extends StatefulWidget {
@@ -19,12 +19,12 @@ class PickDelivery extends StatelessWidget {
   Widget build(BuildContext context) {
     //final List<String> arguments = ModalRoute.of(context).settings.arguments as List<String>;
     final testList = List<String>(10);
-    testList[0] = "Testlist";
-    testList[1] = "newThing";
-    testList[2] = "Monday: 2203";
-    testList[3] = "Testlist";
-    testList[4] = "newThing";
-    testList[5] = "Monday: 2203";
+    testList[0] = "Mon 26/10 15:30-17:30";
+    testList[1] = "Mon 26/10 19:30-21:00";
+    testList[2] = "Tue 27/10 17:00-19:00";
+    testList[3] = "Wed 28/10 17:00-19:00";
+    testList[4] = "Thu 29/10 10:00-12:00";
+    testList[5] = "Wen 26/10 15:30-17:30";
     testList[6] = "Testlist";
     testList[7] = "newThing";
     testList[8] = "Monday: 2203";
@@ -50,7 +50,7 @@ class PickDelivery extends StatelessWidget {
           ),
         ),
         Center(
-          child: Padding(padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Padding(padding: const EdgeInsets.symmetric(horizontal: 40),
 
             child: Button1(
             btnData: ButtonData(
@@ -141,18 +141,19 @@ class TimeslotCardState extends State<TimeslotCard> {
 
   final List<String> timeslots;
   //bool _isSelected = false;
-  Color textColor = Colors.white;
+  //Color textColor = Colors.white;
   TimeslotCardState(this.timeslots);
+  int _value = 1;
+  final String _ecollector = 'Beto';
+
 
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    int _value = 2;
-    print("TIMESLOTTTTTTTTTT##################");
     return Container(
         width: size.width * 0.8,
-        height: 350,
+        height: 430,
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.all(Radius.circular(20)),
@@ -166,38 +167,85 @@ class TimeslotCardState extends State<TimeslotCard> {
           ],
         ),
         child:
-        ListView(
-        children: List<Widget>.generate(this.timeslots.length,
-            (int index) {
-                return ChoiceChip(
-                    //padding: const EdgeInsets.symmetric(horizontal: 100),
-                    label: Text(
-                      this.timeslots[index],
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 20,
-                        color: textColor,
-                      ),
-                    ),
-                    selected: _value == index,
-                    //backgroundColor: widget.chipColor,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    pressElevation: 0,
-                    onSelected: (bool selected) {
-                      //Solo es seleccionable si es un filtro
-                      setState(() {
-                        _value = selected ? index : null;
-                      });
+            Column(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children:[
+              Container(height: 330,child:
+              ListView(
+              children: List<Widget>.generate(this.timeslots.length,
+                  (int index) {
+                      return Column(children:[SizedBox(height: 10,),
+                      ChoiceChip(
+                          label: Container(
+                            width: 240,
+                            height: 40,
+                            child:
+                              Center(child: Text(
+                              this.timeslots[index],
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              )
+                          ),
 
-                    },
-                    /*selectedColor: widget.chipName == 'Recycling Plants Only'
-                        ? TinyColor(widget.chipColor).brighten(50).color
-                        : TinyColor(widget.chipColor).brighten(15).color);}).toList())*/
-                    );
-                  }).toList()
-        )
+                          backgroundColor: Color(0xFFE5E2E2),
+                          selectedColor: Color(0xFFBCBCBC),
+                          selectedShadowColor: Colors.black,
+                          selected: _value == index,
+                          materialTapTargetSize: MaterialTapTargetSize.values[1],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          pressElevation: 0,
+                          onSelected: (bool selected) {
+                            setState(() {
+                              //_value = selected ? index : null;
+                              _value = selected ? index : null;
+                            });
+
+                          },
+                          )
+                      ]
+                      );
+
+              }
+              ).toList()
+              )
+              ),
+              Container(
+                width: 260,
+                height: 50,
+                  decoration: BoxDecoration(
+                    color: BROWN_DARK,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    /*boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 1,
+                        //blurRadius: 6,
+                        //offset: Offset(0, 3),
+                      )
+                    ],*/
+                  ),
+                child:
+                  Center(child: Padding(padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(
+                  "Arrange personally with $_ecollector",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                  ),
+                  )
+                  )
+              ),
+                  SizedBox(height: 0)
+              ]
+            )
     );
   }
 }
