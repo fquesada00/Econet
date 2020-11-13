485aa4d654dd1f6c3b13384f5a9840956cbd6c1a
leobjorkman <leobj@kth.se>
2020-11-13 12:41:52 +0100

    Materials in EcopointInfo now scrollable

--- lib/views/GMap/EcopointInfo.dart
+++ lib/views/GMap/EcopointInfo.dart
@@ -15,7 +15,7 @@ class EcopointInfo extends StatefulWidget {
 class EcopointInfoState extends State<EcopointInfo> {
   //EcopointInfoState({this.adress});
   //String adress;
-  List<String> residues = ['Paper', 'Plastic', 'Glass'];
+  List<String> residues = ['Paper', 'Plastic', 'Glass','Metal','Electronics','Wood','Textile'];
   String ecopointName = "Beto's Ecopoint";
   double distance = 0.2;
   String address = 'Address 1234';
@@ -127,7 +127,7 @@ class EcopointInfoState extends State<EcopointInfo> {
                     width: 10,
                   ),
                   Container(
-                      padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                      padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10.0),
                         color: Color(0xFFe5e2e2),
@@ -136,15 +136,15 @@ class EcopointInfoState extends State<EcopointInfo> {
                       height: 76,
                       width: 285,
                       alignment: Alignment(0, 0),
-                      child: Wrap(
-                        runSpacing: 5,
+                      child: SingleChildScrollView(child:Wrap(
+                        runSpacing: 0,
                         spacing: 5,
                         alignment: WrapAlignment.center,
                         children: residues
                             .map((residue) =>
                                 EconetChip(residue, CHIP_DATA[residue], false))
                             .toList(),
-                      )),
+                      ))),
                 ],
               ),
             ),
